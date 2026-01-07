609906c6f310b1bf13c0a50ac31c05424f799409
wangzaijun <wangzaijun@sensetime.com>
2026-01-07 07:09:20 +0000

    fix

--- lightllm/models/llama/model.py
+++ lightllm/models/llama/model.py
@@ -21,27 +21,6 @@ from lightllm.utils.dist_utils import get_dp_world_size, get_current_device_id
 logger = init_logger(__name__)
 
 
-class LlamaFlashInferStateExtraInfo:
-    def __init__(self, model):
-        tp_world_size = get_dp_world_size()
-        self.tp_q_head_num = model.config["num_attention_heads"] // tp_world_size
-        self.tp_kv_head_num = max(model.config["num_key_value_heads"] // tp_world_size, 1)
-        head_dim = model.config["hidden_size"] // model.config["num_attention_heads"]
-        self.head_dim = model.config.get("head_dim", head_dim)
-        self.workspace_buffer = torch.empty(512 * 1024 * 1024, dtype=torch.int8, device=get_current_device_id())
-        self.max_seq_length = model.max_seq_length
-        self.kv_indices_buffer = [
-            torch.empty(
-                model.graph_max_batch_size * self.max_seq_length, dtype=torch.int32, device=get_current_device_id()
-            ),
-            torch.empty(
-                model.graph_max_batch_size * self.max_seq_length, dtype=torch.int32, device=get_current_device_id()
-            ),
-        ]
-        self.q_data_type = model.data_type
-        self.kv_data_type = torch.float8_e4m3fn if "offline_calibration_fp8kv" in model.mode else model.data_type
-
-
 @ModelRegistry("llama")
 class LlamaTpPartModel(TpPartBaseModel):
     # weight class
@@ -95,11 +74,7 @@ class LlamaTpPartModel(TpPartBaseModel):
         return
 
     def _init_inferstate_cls(self):
-        if get_env_start_args().enable_fa3:
-            self.infer_state_class = FlashAttentionStateInfo
-        elif self.enable_flashinfer:
-            self.infer_state_class = LlamaFlashInferStateInfo
-            self.flashinfer_extra_state = LlamaFlashInferStateExtraInfo(self)
+        pass
 
     def _init_custom(self):
         """
