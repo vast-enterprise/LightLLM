b5763aaa0fb16fb078c8e83acb3cb09e4aa67752
wangzaijun <wzjhelloworld@qq.com>
2026-01-07 12:40:27 +0000

    fix

--- lightllm/common/basemodel/attention/fp8_fa3_backend.py
+++ lightllm/common/basemodel/attention/fp8_fa3_backend.py
@@ -10,6 +10,7 @@ from lightllm.common.basemodel.triton_kernel.q_per_head_fp8_quant import q_per_h
 from lightllm.common.basemodel.triton_kernel.gen_prefill_params import gen_cumsum_pad0_tensor
 from lightllm.utils.vllm_utils import HAS_VLLM, vllm_ops
 from typing import Union
+from .fa3_backend import Fa3AttBackend, Fa3PrefillAttState, Fa3DecodeAttState
 
 if HAS_VLLM:
     scaled_fp8_quant = vllm_ops.scaled_fp8_quant
@@ -17,26 +18,9 @@ else:
     scaled_fp8_quant = None
 
 
-class Fp8Fa3AttBackend(BaseAttBackend):
+class Fp8Fa3AttBackend(Fa3AttBackend):
     def __init__(self, model):
         super().__init__(model=model)
-        self.get_page_table_buffer()  # init
-
-    def get_page_table_buffer(self):
-        """
-        用于减少 decode graph 捕获的时候, 造成显存二次方增长的情况.
-        """
-        model = self.model
-        if not hasattr(self, "_shared_page_table_buffer"):
-            self._shared_page_table_buffer = [
-                torch.empty(model.graph_max_batch_size * model.graph_max_len_in_batch, dtype=torch.int32).to(
-                    get_current_device_id()
-                ),
-                torch.empty(model.graph_max_batch_size * model.graph_max_len_in_batch, dtype=torch.int32).to(
-                    get_current_device_id()
-                ),
-            ]
-        return self._shared_page_table_buffer
 
     def create_att_prefill_state(self, infer_state) -> "Fp8Fa3PrefillAttState":
         return Fp8Fa3PrefillAttState(backend=self, infer_state=infer_state)
@@ -44,41 +28,16 @@ class Fp8Fa3AttBackend(BaseAttBackend):
     def create_att_decode_state(self, infer_state) -> "Fp8Fa3DecodeAttState":
         return Fp8Fa3DecodeAttState(backend=self, infer_state=infer_state)
 
-    def _find_layer_index(
-        self, k: torch.Tensor, v: torch.Tensor, att_state: Union["Fp8Fa3PrefillAttState", "Fp8Fa3DecodeAttState"]
-    ) -> int:
-        kv_buffer = att_state.infer_state.mem_manager.kv_buffer
-        layer_count = len(kv_buffer)
-        find_dict = {kv_buffer[i].data_ptr(): i for i in range(layer_count)}
-        key = min(k.data_ptr(), v.data_ptr())
-        assert key in find_dict
-        return find_dict[key]
-
 
 @dataclasses.dataclass
-class Fp8Fa3PrefillAttState(BasePrefillAttState):
-    cu_seqlens_q: torch.Tensor = None
-    cu_seqlens_k: torch.Tensor = None
-    page_table: torch.Tensor = None
+class Fp8Fa3PrefillAttState(Fa3PrefillAttState):
     # 临时共享变量
     mid_token_batch_ids: torch.Tensor = None
     k_descale: torch.Tensor = None
     v_descale: torch.Tensor = None
 
     def init_state(self):
-        self.cu_seqlens_q = self.infer_state.b1_cu_q_seq_len.int()
-        self.cu_seqlens_k = self.infer_state.b1_cu_kv_seq_len.int()
-        self.page_table = torch.empty(
-            (self.infer_state.batch_size, self.infer_state.max_kv_seq_len),
-            dtype=torch.int32,
-            device=self.infer_state.input_ids.device,
-        )
-        self.page_table.copy_(
-            self.infer_state.req_manager.req_to_token_indexs[
-                self.infer_state.b_req_idx, : self.infer_state.max_kv_seq_len
-            ]
-        )
-
+        super().init_state()
         device = self.infer_state.input_ids.device
         batch_size = self.infer_state.batch_size
         mem_manager = self.backend.model.mem_manager
@@ -168,76 +127,17 @@ class Fp8Fa3PrefillAttState(BasePrefillAttState):
 
 
 @dataclasses.dataclass
-class Fp8Fa3DecodeAttState(BaseDecodeAttState):
-    cu_seqlens_q: torch.Tensor = None
-    cu_seqlens_k: torch.Tensor = None
-    page_table: torch.Tensor = None
-    b_att_seq_len: torch.Tensor = None
-    # 在是否开启mtp 的不同模式下，其设置不同的值，可以加速算子的运行。
-    decode_max_q_seq_len: int = None
-
+class Fp8Fa3DecodeAttState(Fa3DecodeAttState):
     k_descale: torch.Tensor = None
     v_descale: torch.Tensor = None
 
     def init_state(self):
+        super().init_state()
         self.backend: Fp8Fa3AttBackend = self.backend
 
         args_mtp_step = get_env_start_args().mtp_step
-        if args_mtp_step > 0:
-            # 修正 mtp 在 fa3 下的输入。
-            mtp_size = args_mtp_step + 1
-            b_q_seq_len = torch.full(
-                (self.infer_state.b_seq_len.shape[0] // mtp_size,),
-                fill_value=mtp_size,
-                dtype=torch.int32,
-                device=self.infer_state.b_seq_len.device,
-            )
-            b_kv_seq_len = self.infer_state.b_seq_len[mtp_size - 1 :: mtp_size]
-            b1_cu_q_seq_len, b1_cu_kv_seq_len = gen_cumsum_pad0_tensor(
-                b_q_seq_len, b_kv_seq_len[mtp_size - 1 :: mtp_size]
-            )
-            self.cu_seqlens_q = b1_cu_q_seq_len.int()
-            self.cu_seqlens_k = b1_cu_kv_seq_len.int()
-        else:
-            self.cu_seqlens_q = self.infer_state.b1_cu_q_seq_len.int()
-            self.cu_seqlens_k = self.infer_state.b1_cu_kv_seq_len.int()
-
         att_batch_size = self.infer_state.batch_size // (args_mtp_step + 1)
         assert self.infer_state.batch_size % (args_mtp_step + 1) == 0
-
-        model = self.backend.model
-        # 可以使用 cuda graph的时候从 buffer中申请
-        if (
-            self.infer_state.batch_size <= model.graph_max_batch_size
-            and self.infer_state.max_kv_seq_len <= model.graph_max_len_in_batch
-        ):
-            page_buffer = self.backend.get_page_table_buffer(model.graph_max_batch_size, model.graph_max_len_in_batch)
-            self.page_table = page_buffer[self.infer_state.microbatch_index][
-                : att_batch_size * model.graph_max_len_in_batch
-            ].reshape(att_batch_size, model.graph_max_len_in_batch)
-        else:
-            self.page_table = torch.empty(
-                (att_batch_size, self.infer_state.max_kv_seq_len),
-                dtype=torch.int32,
-                device=self.infer_state.input_ids.device,
-            )
-
-        if args_mtp_step > 0:
-            page_table_copy(
-                page_table=self.page_table[:, : self.infer_state.max_kv_seq_len],
-                req_to_token_indexs=model.req_manager.req_to_token_indexs,
-                b_req_idx=self.infer_state.b_req_idx[args_mtp_step :: (args_mtp_step + 1)],
-            )
-            self.b_att_seq_len = self.infer_state.b_seq_len[args_mtp_step :: (args_mtp_step + 1)].contiguous()
-            self.decode_max_q_seq_len = args_mtp_step + 1
-        else:
-            page_table_copy(
-                page_table=self.page_table[:, : self.infer_state.max_kv_seq_len],
-                req_to_token_indexs=model.req_manager.req_to_token_indexs,
-                b_req_idx=self.infer_state.b_req_idx,
-            )
-            self.b_att_seq_len = self.infer_state.b_seq_len
-            self.decode_max_q_seq_len = 1
 
         device = self.infer_state.input_ids.device
         batch_size = att_batch_size
